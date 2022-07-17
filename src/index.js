90122c7e1ff2065278ce03cc57b7c64a0c0ee514
Vladimir <vovchik.niko@gmail.com>
2022-07-17 10:42:24 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+	str = String(n);
+	if (str[0] === "-"){
+		str = String(+str * (-1));
+		str = str.split('').reverse().join('');
+		return str
+	} else {
+		return str.split('').reverse().join('')
+	}
 }
